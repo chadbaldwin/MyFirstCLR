3244ed5c6973d45909937aaa33b1738af09a45b8
Chad Baldwin <chad@chadbaldwin.net>
2022-02-05 19:58:14 -0800

    init

---
+++ CLR.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Text.RegularExpressions;
+
+namespace MyFirstCLR
+{
+    public class CLR
+    {
+        [SqlFunction(IsDeterministic = true, DataAccess = DataAccessKind.None)]
+        public static string ReplaceRegex(string input, string pattern, string replacement)
+            => (input == null || pattern == null || replacement == null) ? null : Regex.Replace(input, pattern, replacement);
+
+        [SqlFunction(IsDeterministic = true, DataAccess = DataAccessKind.None)]
+        public static bool? IsRegexMatch(string input, string pattern)
+            => (input == null || pattern == null) ? (bool?)null : Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase);
+    }
+}
