2f13b9c50bf4714735c5a9f5e62827367198b9a3
Chad Baldwin <chad@chadbaldwin.net>
2022-02-05 20:07:27 -0800

    remove namespace

--- CLR.cs
+++ CLR.cs
@@ -1,16 +1,13 @@
 ﻿using Microsoft.SqlServer.Server;
 using System.Text.RegularExpressions;
 
-namespace MyFirstCLR
+public class CLR
 {
-    public class CLR
-    {
-        [SqlFunction(IsDeterministic = true, DataAccess = DataAccessKind.None)]
-        public static string ReplaceRegex(string input, string pattern, string replacement)
-            => (input == null || pattern == null || replacement == null) ? null : Regex.Replace(input, pattern, replacement);
+    [SqlFunction(IsDeterministic = true, DataAccess = DataAccessKind.None)]
+    public static string ReplaceRegex(string input, string pattern, string replacement)
+        => (input == null || pattern == null || replacement == null) ? null : Regex.Replace(input, pattern, replacement);
 
-        [SqlFunction(IsDeterministic = true, DataAccess = DataAccessKind.None)]
-        public static bool? IsRegexMatch(string input, string pattern)
-            => (input == null || pattern == null) ? (bool?)null : Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase);
-    }
+    [SqlFunction(IsDeterministic = true, DataAccess = DataAccessKind.None)]
+    public static bool? IsRegexMatch(string input, string pattern)
+        => (input == null || pattern == null) ? (bool?)null : Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase);
 }
